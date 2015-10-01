04ca4e66fc5adba7e65bde4d54a4bc00b2f0983e
vincent lambrechts <vinclamb@D065-PC8.ensea.fr>
2015-10-01 17:42:45 +0200

    Diagramme d'état/textures/Sons

---
+++ build/bin/helloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/../bin/helloWorld.dir/src/main.cpp.o"
+  "../bin/helloWorld.pdb"
+  "../bin/helloWorld"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/../bin/helloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+	
+	std::cout << "Salut Patrick !" << std::endl;
+	return 0;
+	}
