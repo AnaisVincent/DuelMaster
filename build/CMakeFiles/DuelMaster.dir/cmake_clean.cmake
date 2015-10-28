a3a69855bf491f7bc653b21b43f6871cadf6dbe8
vincent lambrechts <vinclamb@D067-PC1.smbedu.ensea.fr>
2015-10-28 16:46:03 +0100

    commit linux CentOS

---
+++ build/CMakeFiles/DuelMaster.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/DuelMaster.dir/src/main.cpp.o"
+  "../bin/Debug/DuelMaster.pdb"
+  "../bin/Debug/DuelMaster"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/DuelMaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
